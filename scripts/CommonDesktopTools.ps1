6c69f60cfbfaf1fe94aa14052d5adf3c897fdf69
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 10:34:10 -0700

    Remove unnecessary package installations from script

--- scripts/CommonDesktopTools.ps1
+++ scripts/CommonDesktopTools.ps1
@@ -8,12 +8,3 @@ choco install -y wavelink
 choco install -y elgato-camerahub
 choco install -y wireguard
 choco insgall -y snagit
-choco install -y firacode
-choco install -y terraform
-choco install -y oh-my-posh
-choco install -y terminal-icons.powershell
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python3
-choco install -y 7zip.install
-choco install -y sysinternals
-choco install -y httpie-desktop
