c654d351c7c273f1657a84f025e6cb189c7ebaef
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 10:32:47 -0700

    Add wallpaper setting functionality

--- fulldevice.ps1
+++ fulldevice.ps1
@@ -25,27 +25,18 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
+executeScript "CommonDesktopTools.ps1";
+executeScript "CommonBrowsers.ps1;
 
-#--- Tools ---
-#--- Installing VS and VS Code with Git
-# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
-# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
-# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
-# visualstudio2017community
-# visualstudio2017professional
-# visualstudio2017enterprise
-
-choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
-Update-SessionEnvironment #refreshing env due to Git install
-
-#--- UWP Workload and installing Windows Template Studio ---
-choco install -y visualstudio2017-workload-azure
-choco install -y visualstudio2017-workload-universal
-choco install -y visualstudio2017-workload-manageddesktop
-choco install -y visualstudio2017-workload-nativedesktop
-
-executeScript "WindowsTemplateStudio.ps1";
-executeScript "GetUwpSamplesOffGithub.ps1";
+# Set a wallpaper : 
+write-host "Setting a nice wallpaper"
+$web_dl = new-object System.Net.WebClient
+$wallpaper_url = "https://www.lsu.edu/communications/images/zoom-backgrounds/lsu-7193-tiger-stadium-sunset.jpg"
+$wallpaper_file = "C:\Users\Public\Pictures\lsu.jpg"
+$web_dl.DownloadFile($wallpaper_url, $wallpaper_file)
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Users\Public\Pictures\lsu.jpb" /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_DWORD /d "6" /f 
+reg add "HKEY_CURRENT_USER\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
 
 #--- reenabling critial items ---
 Enable-UAC
