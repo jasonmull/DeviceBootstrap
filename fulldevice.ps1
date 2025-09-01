f083fc54b0c469dd5e3ecc52146006ba8f483409
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 11:41:25 -0700

    Remove SystemConfiguration script execution

--- fulldevice.ps1
+++ fulldevice.ps1
@@ -14,7 +14,6 @@ function executeScript {
 }
 
 #--- Setting up Windows ---
-executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
