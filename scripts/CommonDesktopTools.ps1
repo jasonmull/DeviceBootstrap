962ef106cf70d1e54198ae3d17e5f2a6aaa2c078
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 10:20:09 -0700

    Add CommonDesktopTools.ps1 for developer tools installation

---
+++ scripts/CommonDesktopTools.ps1
@@ -0,0 +1,19 @@
+# tools we expect devs across many scenarios will want
+choco install -y protonvpn
+choco install -y protonpass
+choco install -y protonmail
+choco install -y vmwareworkstation
+choco install -y streamdeck
+choco install -y wavelink
+choco install -y elgato-camerahub
+choco install -y wireguard
+choco insgall -y snagit
+choco install -y firacode
+choco install -y terraform
+choco install -y oh-my-posh
+choco install -y terminal-icons.powershell
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y python3
+choco install -y 7zip.install
+choco install -y sysinternals
+choco install -y httpie-desktop
