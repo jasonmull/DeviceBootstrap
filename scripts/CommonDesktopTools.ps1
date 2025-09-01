ae5bf1c3053ba08b9c073540c936df7280ab2e97
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 13:27:13 -0700

    Remove installation of protonpass from script

--- scripts/CommonDesktopTools.ps1
+++ scripts/CommonDesktopTools.ps1
@@ -1,6 +1,5 @@
 # tools we expect devs across many scenarios will want
 choco install protonvpn
-choco install protonpass
 choco install protonmail
 choco install vmwareworkstation
 choco install streamdeck
