6af17590bcedf4c45d525643939f093d93d01cd2
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 10:18:13 -0700

    Add additional tools to CommonDevTools.ps1

---
+++ scripts/CommonDevTools.ps1
@@ -0,0 +1,17 @@
+# tools we expect devs across many scenarios will want
+choco install -y vscode
+choco install -y vscode-icons
+choco install -y vscode-powershell
+choco install -y vscode-python
+choco install -y vscode-ansible
+choco install -y vscode-terraform
+choco install -y curl
+choco install -y firacode
+choco install -y terraform
+choco install -y oh-my-posh
+choco install -y terminal-icons.powershell
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y python3
+choco install -y 7zip.install
+choco install -y sysinternals
+choco install -y httpie-desktop
