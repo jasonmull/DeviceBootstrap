7d9d90257f44427f154847457c286374ccd60459
Jason Mull <4792624+jasonmull@users.noreply.github.com>
2025-09-01 10:49:31 -0700

    Remove redundant '-y' from choco install commands

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,17 +1,17 @@
 # tools we expect devs across many scenarios will want
-choco install -y vscode
-choco install -y vscode-icons
-choco install -y vscode-powershell
-choco install -y vscode-python
-choco install -y vscode-ansible
-choco install -y vscode-terraform
-choco install -y curl
-choco install -y firacode
-choco install -y terraform
-choco install -y oh-my-posh
-choco install -y terminal-icons.powershell
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python3
-choco install -y 7zip.install
-choco install -y sysinternals
-choco install -y httpie-desktop
+choco install vscode
+choco install vscode-icons
+choco install vscode-powershell
+choco install vscode-python
+choco install vscode-ansible
+choco install vscode-terraform
+choco install curl
+choco install firacode
+choco install terraform
+choco install oh-my-posh
+choco install terminal-icons.powershell
+choco install git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install python3
+choco install 7zip.install
+choco install sysinternals
+choco install httpie-desktop
